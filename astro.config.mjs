246e1b254898a8bb1ebab19faacfdb485ddbb6a4
LEXDEIT <godhood@proton.me>
2024-12-22 21:01:31 -0600

    refactor: changed ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
-
   output: 'static',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
 });
