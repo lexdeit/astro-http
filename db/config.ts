da0d04fed17e952bdedf7a551f8360423da8d5cf
LEXDEIT <godhood@proton.me>
2024-12-22 22:26:38 -0600

    feat: added db logic

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,18 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable, max } from 'astro:db';
+
+
+const Clients = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    age: column.number(),
+    isActive: column.boolean()
+  }
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Clients
+  }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,20 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
+
+
+
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+
+
+
+	await db.insert(Clients).values([
+		{ name: 'Alice', age: 28, isActive: true },
+		{ name: 'Jose', age: 24, isActive: true },
+		{ name: 'Andres', age: 28, isActive: true },
+		{ name: 'Perez', age: 31, isActive: true },
+		{ name: 'Gonzales', age: 30, isActive: false },
+	]).execute();
+
+	console.log('Seeded database');
 }
